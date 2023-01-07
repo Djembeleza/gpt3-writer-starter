2c30dd7f750d4c7d64f4d22e553afaa5ed1f798f
Karabo <50473875+Djembeleza@users.noreply.github.com>
2023-01-07 11:40:49 +0100

    Added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Teach with Jingles🎵 by Djembeleza" key="title"/>
+        <meta property="og:description" content="Teach your child👶🏿👶🏻🚸 anything by singing simple and easy to remember jingles" key="description"/>
         <meta
           property="og:image"
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,84 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({userInput}),
+    });
+
+    const data = await response.json();
+
+    const {output} = data;
+
+    console.log("OpenAI replied....", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+    setUserInput("");
+  }
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Teach with Jingles🎵 by Djembeleza</title>
+        <meta name="description" content="Teach your child👶🏿👶🏻🚸 anything by singing simple and easy to remember jingles" />
+        
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Teach with Jingles🎵</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Teach your child👶🏿👶🏻🚸 anything by singing simple and easy to remember jingles</h2>
           </div>
         </div>
+
+          <div className="prompt-container">
+          <textarea value={userInput} onChange={onUserChangedText} placeholder="What would topic would you like to teach today?😃" className="prompt-box" />
+          <div className="prompt-buttons">
+          <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+          <div className="generate">
+            {isGenerating ? <span className='loader'></span> : <p>Generate</p>}
+          </div>
+          </a>
+          </div>
+
+          {apiOutput && (
+            <div className='output'>
+              <div className='output-header-container'>
+                <div className='output-header'><h3>Output</h3></div>
+              </div>
+
+              <div className='output-content'>
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+
+        
       </div>
-      <div className="badge-container grow">
+      {/* <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
           target="_blank"
@@ -29,7 +89,7 @@ const Home = () => {
             <p>build with buildspace</p>
           </div>
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
